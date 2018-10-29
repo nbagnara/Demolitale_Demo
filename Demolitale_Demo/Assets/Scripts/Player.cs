4ae248b8395ac814e6b8467ec44541f7f96a98b2
nickolas bagnara <nbagnara@gmail.com>
2018-10-28 18:41:33 -0700

    Added Unity files

---
+++ Demolitale_Demo/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    public float speed = 5f;
+
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        var x = Input.GetAxis("Horizontal") * (Time.deltaTime * speed);
+
+        var y = Input.GetAxis("Vertical") * (Time.deltaTime * speed);
+
+        //transform.Rotate(x, y, 0);
+        transform.Translate(x, y, 0);
+    }
+
+
+}
